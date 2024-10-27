156d351ab59a149ff419d08d94d212d6c3298843
Ibrahim Ame <ame.ibrahim@yahoo.com>
2024-10-27 07:07:08 +0200

    More Changes

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+@app.route('/test', methods=['GET'])
+def test_route():
+    return "Test route is working!"
+
 @app.route('/api/data', methods=['GET', 'POST'])
 def handle_data():
     if request.method == 'GET':
@@ -30,6 +34,10 @@ def handle_data():
                 "status": "error"
             }
         return jsonify(response)
+
+@app.errorhandler(500)
+def internal_error(error):
+    return jsonify({"error": "Internal server error occurred!"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
