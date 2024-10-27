482716aa0a699ce7a472408bb95e0891c79cefe4
Ibrahim Ame <ame.ibrahim@yahoo.com>
2024-10-27 07:14:00 +0200

    Changessss

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,2 +1,8 @@
-# passenger_wsgi.py
-from app import app as application
+import sys
+import os
+
+# Add the application directory to the Python path
+sys.path.insert(0, os.path.dirname(__file__))
+
+# Import the Flask app
+from app import app as application  # Ensure 'app.py' contains your Flask app
